8bd9002d05f8b540462e766ad2e70c5828daefe9
HeshamAbdelalem <hesham.abdelalem1@gmail.com>
2018-07-16 00:58:16 +0200

    feat: Add indexedDB code to the app

--- js/idb.js
+++ js/idb.js
@@ -1,4 +1,58 @@
-let dbPromise =  idb.open('restaurantsDB', 1, function(upgradeDb) {
-      let keyVal = upgradeDb.createObjectStore('restaurantsOS');
-      ketVal.put('world', 'hello');
+openDatabase()
+  .then( addRestaurantsToCache() )
+  .then( showCachedRestaurants() )
+  .then( showCachedRestaurants() )
+  .catch((err) => console.log(`there is an  ${err}`) );
+
+function openDatabase() {
+  if (!navigator.serviceWorker) {
+    return Promise.resolve();
+  }
+  return idb.open('restaurantsDB', 1, (upgradeDb) => {
+    let store = upgradeDb.createObjectStore('restaurant', { keyPath: 'id' });
+  });
+}
+
+function addRestaurantsToCache(restaurants) {
+  openDatabase().then( (db) => {
+    if (!db) return;
+
+    let tx = db.transaction('restaurant', 'readwrite');
+    let store = tx.objectStore('restaurant');
+
+    if(restaurants.length == undefined){
+    	store.put(restaurants);
+    }else{
+    restaurants.forEach( (restaurant) => {
+      store.put(restaurants);
     });
+	}
+    return tx.complete;
+  });
+}
+
+
+function showCachedRestaurants() {
+  return openDatabase().then( (db) => {
+    let tx = db.transaction('restaurants');
+    let store = tx.objectStore('restaurants');
+    return store.getAll();
+
+  }).then( (restaurants) => {
+    return restaurants;
+  });
+}
+
+
+function showCachedRestaurant(id) {
+  return openDatabase().then( (db) => {
+    let tx = db.transaction('restaurant');
+    let store = tx.objectStore('restaurant');
+    id = parseInt(id);
+
+    return store.get(id);
+  }).then( (restaurant) => {
+    console.log(restaurant);
+    return restaurant;
+  });
+}
